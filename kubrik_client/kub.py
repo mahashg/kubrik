35b493f8040f1ddf9ef4a02b21d5c4f8ff756c21
Mahesh Gupta <mahesh.gupta@rubrik.com>
2022-10-11 18:32:39 -0700

    adding register and snapshot command

--- kubrik_client/kub.py
+++ kubrik_client/kub.py
@@ -8,25 +8,48 @@ import logging
 log = logging.getLogger('%s.py' % '/'.join(__name__.split('.')))
 
 
-def process_message():
+def process_register(args):
+    print("process_register called with argument ", args)
+
+
+def process_snapshot(args):
+    print("process_snapshot called with arguments ", args)
     
+def parse_process_register(subparser):
+    parser = cli.add_parser(
+        subparser,
+        'register',
+        help='Register client with server',
+        formatter_class=cli.Formatter,
+    )
+    cli.set_defaults(parser, func=process_register)
+
+
+def parse_process_snapshot(subparser):
+    parser = cli.add_parser(
+        subparser,
+        'snapshot',
+        help='Register snapshot of server',
+        formatter_class=cli.Formatter,
+    )
+    cli.set_defaults(parser, func=process_snapshot)
+
+
 def _parse_args():
     parser = cli.create_parser(__doc__)
-    cli.add_deployment_arguments(parser)
-    cli.add_argument(
+    subparser = cli.add_subparsers(
         parser,
-        '-email',
-        '--email',
-        help='Email address whose activities need to be checked (can be substring)',
-        type=str,
+        dest='op',
+        help='Operation',
     )
-    cli.set_defaults(parser, func=process_message)
+    parse_process_register(subparser) 
+    parse_process_snapshot(subparser)
     return cli.parse_args(parser)
 
 
 def _main():
     args = _parse_args()
-    log.info('This is a dry-run. No actual changes will be made as part of dry run')
+    args.func(args)    
 
 
 
