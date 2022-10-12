bbf5a0c1cb92323ca3cfdd45d6a55204f33bf21f
Mahesh Gupta <mahesh.gupta@rubrik.com>
2022-10-11 20:00:38 -0700

    adding kopia.py command execution command

---
+++ kubrik_client/kopia.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+
+
+log = logging.getLogger('%s.py' % '/'.join(__name__.split('.')))
+
+BASE_COMMAND="./kopia"
+BASE_WITH_CREDENTIAL=BASE_COMMAND + " --credentials-file api.json"
+
+
+def execute_command(args):
+    command = " ".join(args)
+    out = os.popen(command)
+    return out.read()
+
+def validate_provider():
+    """ command needs fixing"""
+    return execute_command([BASE_COMMAND, "repository", "validate-provider"])
+
+def connect(gdrive_id):
+    """connect with gdrive id"""
+    return execute_command([BASE_WITH_CREDENTIAL, "connect", "gdrive", " --folder-id="+gdrive_id])
+
+def snapshot_create(dir):
+    """start taking snapshot of a directory"""
+    return execute_command([BASE_WITH_CREDENTIAL, "snapshot", "create", dir])
+
+def list_repo(dir):
+    """list all the repo which are snapshotted"""
+    return execute_command([BASE_COMMAND, "snapshot", "list"])
+
+def restore(hash_id):
+    """restore the content with id right here"""
+    return execute_command([BASE_COMMAND, "restore", id])
+
+def do_ls(hash_id):
+    """ See content of directory"""
+    return execute_command([BASE_COMMAND, "ls", "-l", id])
+
+def repo_status():
+    return execute_command[BASE_COMMAND, "repository", "status"]
--- kubrik_client/kub.py
+++ kubrik_client/kub.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 
 from rubrik.sdk_internal.cli_tools import cli
-import sys
-import logging
 
 
 log = logging.getLogger('%s.py' % '/'.join(__name__.split('.')))
 
 
 def process_register(args):
+    """    
+    Server internally stores email_id and 
+    uses it future communication with server
+    """
     print("process_register called with argument ", args)
 
 
 def process_snapshot(args):
+    """
+    Server uses email_id, path and passes to server, server returns response
+    which fails if directory is already in snapshot
+    which succeeds and returns google_drive_id
+    """
     print("process_snapshot called with arguments ", args)
-    
+
+
+def process_restore(args):
+    """
+    Server uses email_id, path and returns google_drive_id
+    client and use it to navigate and figure out interaction
+    """
+    print("process_restore called with arguments ", args)
+
+
 def parse_process_register(subparser):
     parser = cli.add_parser(
         subparser,
@@ -22,17 +38,38 @@ def parse_process_register(subparser):
         help='Register client with server',
         formatter_class=cli.Formatter,
     )
+    _add_email_argument(parser)
     cli.set_defaults(parser, func=process_register)
 
+
+def _add_email_argument(parser):
+    cli.add_argument(
+        parser,
+        '-email',
+        '--email',
+        help='Email id of the user',
+        type=str,
+        required=True,
+    )
 
 def parse_process_snapshot(subparser):
     parser = cli.add_parser(
         subparser,
         'snapshot',
-        help='Register snapshot of server',
+        help='Begin taking snapshot of file',
         formatter_class=cli.Formatter,
     )
     cli.set_defaults(parser, func=process_snapshot)
+
+
+def parse_restore_command(subparser):
+    parser = cli.add_parser(
+        subparser,
+        'restore',
+        help='Begin restoring snapshot of a server',
+        formatter_class=cli.Formatter,
+    )
+    cli.set_defaults(parser, func=process_restore)
 
 
 def _parse_args():
@@ -50,7 +87,6 @@ def _parse_args():
 def _main():
     args = _parse_args()
     args.func(args)    
-
 
 
 if __name__ == '__main__':
