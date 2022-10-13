7f04dacc330a2608620327838e5c7c2128958593
Mahesh Gupta <mahesh.gupta@rubrik.com>
2022-10-12 23:56:49 -0700

    move google drive allocation to a separate file

--- kubrik_server/db_init.py
+++ kubrik_server/db_init.py
@@ -6,6 +6,11 @@ connection = sqlite3.connect('kubrik.db')
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
+lines = open("drives.txt", "r").read().split("\n")
+for line in lines:
+    query = query = "INSERT INTO google_drive_ids VALUES ('"+line+"', 1)"
+    connection.execute(query)
+
 cur = connection.cursor()
 connection.commit()
 connection.close()
--- kubrik_server/schema.sql
+++ kubrik_server/schema.sql
@@ -24,10 +24,3 @@ CREATE TABLE repository (
     PRIMARY KEY (gmailid, name),
     FOREIGN KEY(gmailid) REFERENCES uuids(gmailid)
 );
-
-
-INSERT INTO google_drive_ids VALUES ("1WPP_RpY3v9ZPHM5xGHkyf3xyTAZXBEaH", 1);
-INSERT INTO google_drive_ids VALUES ("1ssrdz2EgDRfJ-RHR2S7Q3fB2cd9foX1l", 1);
-INSERT INTO google_drive_ids VALUES ("1g9KaIFglDvACO2FIZsUkCCFYWo40wOL2", 1);
-INSERT INTO google_drive_ids VALUES ("1tMTzXmaBvniVKeHSd4-dFxGnnmMuoQDp", 1);
-INSERT INTO google_drive_ids VALUES ("16vwQ-odNEDMe3n6y34eQXoPbInLSXLSf", 1);
